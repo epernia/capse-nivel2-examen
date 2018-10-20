57f47b2c6af1cfcb61d47c4de03b2f77197834c8
Matias Siracusa <32089@alumnos.frm.utn.edu.ar>
2018-10-20 20:15:21 -0300

    Función scan del keypad funcionando

--- msiracusa/mefIngresoPiso.c
+++ msiracusa/mefIngresoPiso.c
@@ -1,74 +1,146 @@
+#include "board.h"
+#include "sapi.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "mefIngresoPiso.h"
 /* circularBufferUse( pisosPendientes ); */
+#define DEBUG_ENABLED 1
 
-void mefIgresoPisoUpdate(void) {
+void mefIngresoPisoUpdate(void) {
   switch (mefIngresoPisoCurrentState) {
   case EN_ESPERA_DE_DIGITO_1: {
+
     /* inicializo a cero el vector piso */
     piso = 0;
-    if (readKeyboard()) {
-      piso = readKeyboard();
+
+    if (readKeypad()) {
+      piso = readKeypad();
       mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_2_O_LETRA;
       updateLCDDisplayOnKeyPress();
-    } else
-      return;
+    } 
+     
     break;
   }
   case EN_ESPERA_DE_DIGITO_2_O_LETRA: {
-    if (!updateTimer()) // debe regresar segundos restantes;
-      mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
-    if (readKeyboard()) { // debe regresar 0 si es letra no A o B
-      if (readKeyboard == numero)
-        piso = piso * 10 + readKeyboard();
-      if (readKeyboard == A) {
-        mefIngresoPisoCurrentState = GUARDAR_PISO;
-      }
-      if (readKeyboard == B)
-        mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
-      1();
-    }
+    /* if (!updateTimer()) // debe regresar segundos restantes; */
+    /*   mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
+    /* if (readKeypad()) { // debe regresar 0 si es letra no A o B */
+    /*   if (readKeypad == numero) */
+    /*     piso = piso * 10 + readKeypad(); */
+    /*   if (readKeypad == A) { */
+    /*     mefIngresoPisoCurrentState = GUARDAR_PISO; */
+    /*   } */
+    /*   if (readKeypad == B) */
+    /*     mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
+    /*   if (readKeypad != anteriores) */
+    /*     printError */
+    /* } */
     break;
   }
   case EN_ESPERA_DE_LETRA: {
-    if (!updateTimer()) // debe regresar segundos restantes;
-      mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
-    if (readKeyboard == A) {
-      mefIngresoPisoCurrentState = GUARDAR_PISO;
-    }
-    if (readKeyboard == B) {
-      mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
-      cancelTimer();
-    }
+    /* if (!updateTimer()) // debe regresar segundos restantes; */
+    /*   mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
+    /* if (readKeypad == A) { */
+    /*   mefIngresoPisoCurrentState = GUARDAR_PISO; */
+    /* } */
+    /* if (readKeypad == B) { */
+    /*   mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
+    /*   cancelTimer(); */
+    /* } */
     break;
   }
   case GUARDAR_PISO: {
-    circularBufferWrite(&pisosPendientes, &piso);
-    // TODO procesar el buffer y ordenarlo?
-    printLCDMessage();
-    mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
+    /* circularBufferWrite(&pisosPendientes, &piso); */
+    /* // TODO procesar el buffer y ordenarlo? */
+    /* printLCDMessage(); */
+    /* mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
     break;
   }
   default:
     break;
   }
+} 
+
+uint8_t readKeypad(void) {
+  /* user_input guardara el caracter ascii a retornar */
+  uint8_t user_input = {0};
+  
+  for (uint8_t i = 0; i < 4; ++i) {
+    /* coloco en alto las columnas */
+    gpioWrite(keypad_cols[i], ON);
+
+    for (uint8_t j = 0; j < 4; ++j) {
+      /* leo las filas y detecto cual boton esta presionado */
+
+      if (gpioRead(keypad_rows[j])) {
+        /* realizo el mapeo de [filas][columnas] a caracter ascii */
+        user_input = keypad_characters[j][i];
+        /* imprimo por uart si esta habilitado el debug */
+        if (DEBUG_ENABLED) {
+          char str[2] = "\0";
+          str[0] = user_input;
+          uartWriteString(UART_USB, "\n [I] Caracter presionado: ");
+          uartWriteString(UART_USB, str);
+        }
+        /* espero a que se deje de presionar el digito */
+        while (gpioRead(keypad_rows[j])) {
+          delay(10/1000); // delay acepta segundos como parametros
+        }
+      }
+    }
+    gpioWrite(keypad_cols[i], OFF);
+  }
+  return user_input;
 }
 
-uint8_t readKeyboard(){
+uint8_t updateLCDDisplayOnKeyPress() {
   //TODO
-}
-
-uint8_t updateLCDDisplayOnKeyPress(){
-  //TODO
+  return 0;
 }
 
 uint8_t printLCDMessage(char * msg[static 2]){
   //TODO
+  return 0;
 }
 
-uint8_t updateTimer(char * msg[static 2]){
+uint8_t updateTimer(){
   //TODO
+  return 0;
 }
 
-uint8_t cancelTimer(char * msg[static 2]){
+uint8_t cancelTimer(){
   //TODO
+  return 0;
+}
+
+
+// Solamente para pruebas
+int main(){
+  boardConfig();
+  uartConfig(UART_USB, 115200);
+  /* inicializo los GPIOs que se usaran */
+    /* para leer desde el keypad */
+  for (uint8_t i = 0; i < 4; i++) {
+    gpioConfig(keypad_cols[i], GPIO_OUTPUT);
+  }
+  for (uint8_t i = 0; i < 4; i++) {
+    gpioConfig(keypad_rows[i], GPIO_INPUT_PULLDOWN);
+  }
+
+
+  for(;;){
+    // prueba lectura teclado
+    uint8_t myTec = readKeypad();
+    if (myTec){
+      char buff[10];
+      sprintf(buff, "%d", myTec);
+      uartWriteString(UART_USB, "\n [I] Lectura teclado ");
+      
+      uartWriteString(UART_USB, buff);
+    }
+    /* mefIngresoPisoUpdate(); */
+    
+    }
+  return 0;
 }
--- msiracusa/mefIngresoPiso.h
+++ msiracusa/mefIngresoPiso.h
@@ -2,9 +2,15 @@
 #define MEFINGRESOPISO_H 
 
 #include "sapi.h"
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 /* enum que contiene los posibles estados de la MEF*/
-typedef enum{
+typedef enum mefIngresoPisoState{
              EN_ESPERA_DE_DIGITO_1,
              EN_ESPERA_DE_DIGITO_2_O_LETRA,
              EN_ESPERA_DE_LETRA,
@@ -15,15 +21,24 @@ typedef enum{
 /* la variable piso contiene el valor numérico del piso */
 /* seleccionado */
 static uint8_t piso;
+/* variables para recorrer el teclado matricial */
+static const uint8_t keypad_cols[4] = {T_COL0, T_FIL2, T_FIL3, T_FIL0}; //FIXME
+static const uint8_t keypad_rows[4] = {T_COL1, CAN_TD, CAN_RD, RS232_TXD}; //FIXME
 
-
+/* tabla de mapeo: filaxcolumna -> carcter ascii*/
+//FIXME: Cambiar a algo que tenga sentido
+static const char keypad_characters[4][4] = {{'D', '#', '0', '*'}, // 1ra fila
+                                             {'C', '9', '8', '7'}, // 2da fila
+                                             {'B', '6', '5', '4'},
+                                             {'A', '3', '2', '1'}};
+ 
 /* esta variable contiene el estado actual de la MEF */
 mefIngresoPisoState_t mefIngresoPisoCurrentState;
 
 void mefIngresoPisoUpdate( void );
-void readKeyboard();
-void updateLCDDisplayOnKeyPress();
-void printLCDMessage();
+uint8_t readKeypad();
+uint8_t updateLCDDisplayOnKeyPress();
+uint8_t printLCDMessage(char * msg[static 2]);
 uint8_t updateTimer();
 uint8_t cancelTimer();
 
