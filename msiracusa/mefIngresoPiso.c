4e2d03b8b8b1522ebe03c1e76986ad13e11f081c
Matias Siracusa <32089@alumnos.frm.utn.edu.ar>
2018-10-21 18:05:14 -0300

    Completado función timer, mejorado el logging, estados

--- msiracusa/mefIngresoPiso.c
+++ msiracusa/mefIngresoPiso.c
@@ -1,56 +1,102 @@
+#include "mefIngresoPiso.h"
 #include "board.h"
 #include "sapi.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "mefIngresoPiso.h"
 /* circularBufferUse( pisosPendientes ); */
-#define DEBUG_ENABLED 1
+#define TIMEOUT 5
 
 void mefIngresoPisoUpdate(void) {
   switch (mefIngresoPisoCurrentState) {
   case EN_ESPERA_DE_DIGITO_1: {
 
-    /* inicializo a cero el vector piso */
-    piso = 0;
-
-    if (readKeypad()) {
-      piso = readKeypad();
+    printLCDMessage("* bienvenida*");
+    piso = readKeypad();
+    if (piso >= 0 && piso <= 9) {
+      if ( debugLevel >= (1 << INFO)) {
+        uartWriteString(UART_USB,
+                        "\n [I] EN_ESPERA_DE_DIGITO_1: Leido valor numerico");
+        uartWriteString(UART_USB, "\n [I] EN_ESPERA_DE_DIGITO_1: Transicion a "
+                                  "EN_ESPERA_DE_DIGITO_2_O_LETRA");
+      }
       mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_2_O_LETRA;
-      updateLCDDisplayOnKeyPress();
-    } 
-     
+      updateLCDDisplayOnKeyPress(/* digito */);
+    } else {
+      updateLCDDisplayOnKeyPress(/* valor invalido */);
+    }
+
     break;
   }
   case EN_ESPERA_DE_DIGITO_2_O_LETRA: {
-    /* if (!updateTimer()) // debe regresar segundos restantes; */
-    /*   mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
-    /* if (readKeypad()) { // debe regresar 0 si es letra no A o B */
-    /*   if (readKeypad == numero) */
-    /*     piso = piso * 10 + readKeypad(); */
-    /*   if (readKeypad == A) { */
-    /*     mefIngresoPisoCurrentState = GUARDAR_PISO; */
-    /*   } */
-    /*   if (readKeypad == B) */
-    /*     mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
-    /*   if (readKeypad != anteriores) */
-    /*     printError */
-    /* } */
+    /* actualiza el valor del timer y lo guarda en la var. global timer */
+    updateTimer();
+
+    /* si timer llega a cero, vuelve al estado anterior e imprime un mensaje de
+     * timeout */
+    if (!timer) {
+      mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
+      printLCDMessage("* timeout *");
+      if (debugLevel >= (1 << INFO))
+        uartWriteString(UART_USB, "\n [I] EN_ESPERA_DE_DIGITO_2_O_LETRA: El "
+                                  "usuario no ingreso un valor. Timeout");
+      break;
+    }
+
+    uint8_t pisoTemp = readKeypad(); /* correccion Chr -> Dec */
+    if (pisoTemp >= 0 && pisoTemp <= 9) {
+      piso = piso * 10 + pisoTemp;
+      mefIngresoPisoCurrentState = EN_ESPERA_DE_LETRA;
+      if (debugLevel >= (1 << INFO)) {
+        uartWriteString(
+            UART_USB,
+            "\n [I] EN_ESPERA_DE_DIGITO_2_O_LETRA: Transicion a GUARDAR_PISO");
+      }
+    } else {
+      if (pisoTemp == 17) { /* usuario presiona 'A' */
+        mefIngresoPisoCurrentState = GUARDAR_PISO;
+        if (debugLevel >= (1 << INFO))
+          uartWriteString(
+              UART_USB,
+              "\n [I] EN_ESPERA_DE_DIGITO_2_O_LETRA: Transicion a GUARDAR_PISO");
+      } else {
+        if (readKeypad == 18) { /* usuario presiona 'B' */
+          mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
+          if (debugLevel >= (1 << INFO)) {
+            uartWriteString(UART_USB,
+                            "\n [I] EN_ESPERA_DE_DIGITO_2_O_LETRA: Ingreso de "
+                            "piso cancelado por el usuario");
+            uartWriteString(UART_USB, "\n [I] EN_ESPERA_DE_DIGITO_2_O_LETRA: "
+                                      "Transicion a EN_ESPERA_DE_DIGITO_1");
+          }
+        } else {
+          printLCDMessage("*opcion invalida*");
+        }
+      }
+    }
     break;
   }
   case EN_ESPERA_DE_LETRA: {
-    /* if (!updateTimer()) // debe regresar segundos restantes; */
-    /*   mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
+    if (!updateTimer()) // debe regresar segundos restantes; */
+      mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1;
     /* if (readKeypad == A) { */
-    /*   mefIngresoPisoCurrentState = GUARDAR_PISO; */
+      /* mefIngresoPisoCurrentState = GUARDAR_PISO; */
     /* } */
     /* if (readKeypad == B) { */
-    /*   mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
-    /*   cancelTimer(); */
+      /* mefIngresoPisoCurrentState = EN_ESPERA_DE_DIGITO_1; */
+      /* cancelTimer(); */
     /* } */
     break;
   }
   case GUARDAR_PISO: {
+    cancelTimer();
+    if (debugLevel >= (1 << INFO)) {
+      char * buff[10];
+      sprintf(buff, "%d", piso);
+      uartWriteString(UART_USB,
+                      "\n [I] GUARDAR_PISO: Valor de piso a almacenar: ");
+      uartWriteString(UART_USB, buff);
+    }
     /* circularBufferWrite(&pisosPendientes, &piso); */
     /* // TODO procesar el buffer y ordenarlo? */
     /* printLCDMessage(); */
@@ -60,12 +106,12 @@ void mefIngresoPisoUpdate(void) {
   default:
     break;
   }
-} 
+}
 
 uint8_t readKeypad(void) {
   /* user_input guardara el caracter ascii a retornar */
   uint8_t user_input = {0};
-  
+
   for (uint8_t i = 0; i < 4; ++i) {
     /* coloco en alto las columnas */
     gpioWrite(keypad_cols[i], ON);
@@ -77,50 +123,75 @@ uint8_t readKeypad(void) {
         /* realizo el mapeo de [filas][columnas] a caracter ascii */
         user_input = keypad_characters[j][i];
         /* imprimo por uart si esta habilitado el debug */
-        if (DEBUG_ENABLED) {
+        if (debugLevel >= (1 << INFO)) {
           char str[2] = "\0";
           str[0] = user_input;
-          uartWriteString(UART_USB, "\n [I] Caracter presionado: ");
+          uartWriteString(UART_USB,
+                          "\n [I] readKeypad :: Caracter presionado: ");
           uartWriteString(UART_USB, str);
         }
         /* espero a que se deje de presionar el digito */
         while (gpioRead(keypad_rows[j])) {
-          delay(10/1000); // delay acepta segundos como parametros
+          delay(10 / 1000); // delay acepta segundos como parametros
         }
       }
     }
     gpioWrite(keypad_cols[i], OFF);
   }
-  return user_input;
+
+  /* correccion chr a decimal, de esta manera un '0' es igual  0. */
+  return user_input - 48;
 }
 
 uint8_t updateLCDDisplayOnKeyPress() {
-  //TODO
+  // TODO
   return 0;
 }
 
-uint8_t printLCDMessage(char * msg[static 2]){
-  //TODO
+uint8_t printLCDMessage(char *msg[static 2]) {
+  // TODO
   return 0;
 }
 
-uint8_t updateTimer(){
-  //TODO
+uint8_t updateTimer() {
+  if (context & (1 << TIMER_STARTED))
+    timer--;
+  else { /* inicializo el timer  */
+    timer = TIMEOUT;
+    context |= (1 << TIMER_STARTED);
+  }
+  if (!timer)
+    context &= (0 << TIMER_STARTED);
+  if (debugLevel >= (1 << DEBUG)) {
+    char *buff[10];
+    uartWriteString(UART_USB, "\n [I] updateTimer :: Valor del timer: ");
+    sprintf(buff, "%d", timer);
+    uartWriteString(UART_USB, buff);
+  }
+  // FIXME : Quitar
+  delay(199);
+  return timer;
+}
+
+uint8_t cancelTimer() {
+  // TODO
   return 0;
 }
 
-uint8_t cancelTimer(){
-  //TODO
-  return 0;
-}
-
-
-// Solamente para pruebas
-int main(){
+// FIXME: Solamente para pruebas
+// FIXME: Si me queda tiempo, hacer un test con el main
+int main() {
   boardConfig();
   uartConfig(UART_USB, 115200);
+  tickConfig(1);
+  debugLevel |= (1 << INFO);
+  uartWriteString(UART_USB, "\n [I] Nivel debug:  ");
+  char * buff[10];
+  sprintf(buff, "%d", debugLevel);
+  uartWriteString(UART_USB, buff);
+
   /* inicializo los GPIOs que se usaran */
-    /* para leer desde el keypad */
+  /* para leer desde el keypad */
   for (uint8_t i = 0; i < 4; i++) {
     gpioConfig(keypad_cols[i], GPIO_OUTPUT);
   }
@@ -128,19 +199,16 @@ int main(){
     gpioConfig(keypad_rows[i], GPIO_INPUT_PULLDOWN);
   }
 
-
-  for(;;){
+  for (;;) {
     // prueba lectura teclado
     uint8_t myTec = readKeypad();
-    if (myTec){
+    if (myTec) {
       char buff[10];
       sprintf(buff, "%d", myTec);
-      uartWriteString(UART_USB, "\n [I] Lectura teclado ");
-      
-      uartWriteString(UART_USB, buff);
+      /* uartWriteString(UART_USB, "\n [I] Lectura teclado "); */
+      /* uartWriteString(UART_USB, buff); */
     }
-    /* mefIngresoPisoUpdate(); */
-    
-    }
+    mefIngresoPisoUpdate();
+  }
   return 0;
 }
--- msiracusa/mefIngresoPiso.h
+++ msiracusa/mefIngresoPiso.h
@@ -1,5 +1,5 @@
-#ifndef MEFINGRESOPISO_H 
-#define MEFINGRESOPISO_H 
+#ifndef MEFINGRESOPISO_H
+#define MEFINGRESOPISO_H
 
 #include "sapi.h"
 #include <stdlib.h>
@@ -8,40 +8,50 @@
 extern "C" {
 #endif
 
-
 /* enum que contiene los posibles estados de la MEF*/
-typedef enum mefIngresoPisoState{
-             EN_ESPERA_DE_DIGITO_1,
-             EN_ESPERA_DE_DIGITO_2_O_LETRA,
-             EN_ESPERA_DE_LETRA,
-             GUARDAR_PISO,
-             NUM_ESTADOS,
+typedef enum mefIngresoPisoState {
+  EN_ESPERA_DE_DIGITO_1,
+  EN_ESPERA_DE_DIGITO_2_O_LETRA,
+  EN_ESPERA_DE_LETRA,
+  GUARDAR_PISO,
+  NUM_ESTADOS,
 } mefIngresoPisoState_t;
 
-/* la variable piso contiene el valor numérico del piso */
-/* seleccionado */
+enum mefFlags {
+  TIMER_STARTED,
+  NUM_FLAGS,
+}; 
+
+  enum debugLevel {
+                           WARN,
+                           INFO,
+                           DEBUG,
+  };
+/* la variable piso contiene el valor numerico del piso seleccionado */
 static uint8_t piso;
+/* la variable timer contiene el valor numerico para cuentas regresivas */
+static uint8_t timer;
+/* almacena el valor actual de las banderas definidas en enum mefFlags */
+static uint8_t context;
+  static uint8_t debugLevel;
 /* variables para recorrer el teclado matricial */
-static const uint8_t keypad_cols[4] = {T_COL0, T_FIL2, T_FIL3, T_FIL0}; //FIXME
-static const uint8_t keypad_rows[4] = {T_COL1, CAN_TD, CAN_RD, RS232_TXD}; //FIXME
+static const uint8_t keypad_cols[4] = {T_COL0, T_FIL2, T_FIL3, T_FIL0};
+static const uint8_t keypad_rows[4] = {T_COL1, CAN_TD, CAN_RD, RS232_TXD};
 
 /* tabla de mapeo: filaxcolumna -> carcter ascii*/
-//FIXME: Cambiar a algo que tenga sentido
 static const char keypad_characters[4][4] = {{'D', '#', '0', '*'}, // 1ra fila
                                              {'C', '9', '8', '7'}, // 2da fila
                                              {'B', '6', '5', '4'},
                                              {'A', '3', '2', '1'}};
- 
-/* esta variable contiene el estado actual de la MEF */
+
+/*contiene el estado actual de la MEF */
 mefIngresoPisoState_t mefIngresoPisoCurrentState;
 
-void mefIngresoPisoUpdate( void );
+void mefIngresoPisoUpdate(void);
 uint8_t readKeypad();
 uint8_t updateLCDDisplayOnKeyPress();
-uint8_t printLCDMessage(char * msg[static 2]);
+uint8_t printLCDMessage(char *msg[static 2]);
 uint8_t updateTimer();
 uint8_t cancelTimer();
-
-
 
 #endif /*INGRESOPISO.H*/
